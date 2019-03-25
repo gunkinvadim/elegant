429503b3a1c7cb1a1a61b36358a5dd301b15ca69
gunkinvadim <46325759+gunkinvadim@users.noreply.github.com>
2019-03-26 00:07:38 +0300

    0.02

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="css/reset.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 				<!-- fonts -->
    	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,600&amp;subset=cyrillic-ext" rel="stylesheet">
@@ -40,13 +41,62 @@
 					<h1 class="main-title">
 						Say <span>Haloa</span> to your Portfolio
 					</h1>
-					<a class="main-arrow wow flash" href="#"><img src="img/main/arrow-down.png" alt="Down"></a>
+					<a class="main-arrow wow flash" href="#about"><img src="img/main/arrow-down.png" alt="Down"></a>
 				</section>
 				<!-- /.main -->
 
+			<section id="about" class="section about">
+				<div class="container">
+					<h2 class="section-title">Who we are</h2>
+					<!-- /.section-title -->
+					<h3 class="section-subtitle">Tell The World About Yourself</h3>
+					<!-- /.section-subtitle -->
+					<p class="section-description">Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc, fiant sollemnes in futurum.</p>
+					<!-- /.section-description -->
+					<button class="button">Read More About Us</button>
+				</div>
+				<!-- /.container -->
+			</section>
+			<!-- /.about -->
 
-				<!-- JQuery -->
-    <script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+			<section class="section projects">
+				<h2 class="section-title">What we do</h2>
+				<!-- /.section-title -->
+				<h3 class="section-subtitle">Show Your Amazing Work</h3>
+				<!-- /.section-subtitle -->
+
+				<div class="projects-menu">
+					<ul class="projects-menu__block">
+						<li class="projects-menu__item"><a href="#"><img src="img/projects/project-1.jpg" alt="Project-1"></a></li>
+						<!-- /.projects-menu__item -->
+						<li class="projects-menu__item"><a href="#"><img src="img/projects/project-2.jpg" alt="Project-2"></a></li>
+						<!-- /.projects-menu__item -->
+						<li class="projects-menu__item"><a href="#"><img src="img/projects/project-3.jpg" alt="Project-3"></a></li>
+						<!-- /.projects-menu__item -->
+						<li class="projects-menu__item"><a href="#"><img src="img/projects/project-4.jpg" alt="Project-4"></a></li>
+						<!-- /.projects-menu__item -->
+					</ul>
+					<ul class="projects-menu__block">
+						<li class="projects-menu__item"><a href="#"><img src="img/projects/project-5.jpg" alt="Project-5"></a></li>
+						<!-- /.projects-menu__item -->
+						<li class="projects-menu__item"><a href="#"><img src="img/projects/project-6.jpg" alt="Project-6"></a></li>
+						<!-- /.projects-menu__item -->
+						<li class="projects-menu__item"><a href="#"><img src="img/projects/project-7.jpg" alt="Project-7"></a></li>
+						<!-- /.projects-menu__item -->
+						<li class="projects-menu__item"><a href="#"><img src="img/projects/project-8.jpg" alt="Project-8"></a></li>
+						<!-- /.projects-menu__item -->
+					</ul>
+					<!-- /.projects-menu__block -->
+				</div>
+				<!-- /.projects-menu -->
+			</section>
+			<!-- /.section projects -->
+
+				<!-- Optional JavaScript -->
+				<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+				<script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+				<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+				<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 				<!-- Custom scripts -->
     <script src="js/script.js"></script>
