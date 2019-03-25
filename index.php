7f2345617fc2f91d73b9e0f093b1cb66127cf64a
gunkinvadim <46325759+gunkinvadim@users.noreply.github.com>
2019-03-25 15:09:11 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="ru">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="css/reset.css">
+
+    <!-- fonts -->
+   	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,600&amp;subset=cyrillic-ext" rel="stylesheet">
+
+   	<!-- Animate CSS -->
+   	<link rel="stylesheet" href="css/animate.css">
+
+   	<!-- WOW -->
+    <script src="js/wow.min.js"></script>
+    <script>
+				 new WOW().init();
+				</script>
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>ELEGANT</title>
+  </head>
+  <body>
+
+				<header class="header">
+					<div class="header-logo">
+						<img src="img/header/header-logo.png" alt="Elegant">
+					</div>
+					<!-- /.header-logo -->
+					<button class="header-button">
+						<span class="hidden-mobile">MENU</span>
+						<img src="img/header/header-menu-icon.png" alt="Меню" class="header-button__icon">
+					</button>
+					<!-- /.header-button -->
+				</header>
+
+				<section class="main">
+					<h1 class="main-title">
+						Say <span>Haloa</span> to your Portfolio
+					</h1>
+					<a class="main-arrow wow flash" href="#"><img src="img/main/arrow-down.png" alt="Down"></a>
+				</section>
+				<!-- /.main -->
+
+
+				<!-- JQuery -->
+    <script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+
+				<!-- Custom scripts -->
+    <script src="js/script.js"></script>
+  </body>
+</html>
